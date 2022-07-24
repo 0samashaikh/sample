862e69f3328c8ddac362f30a2bb656bb69e39159
Osama Shaikh <shaikhosama504@gmail.com>
2022-07-24 20:58:29 +0530

    socket io

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+let http = require('http');
+
 
 app.use(express.static(__dirname + '/dist/sample'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/sample/index.html'));
 });
 
-app.listen(process.env.PORT || 3000)
+let server = http.Server(app);
+
+
+let io = require('socket.io')(server, { cors: { origin: "*" } });
+
+
+server.listen(process.env.PORT || 3000, ()=>{
+    console.log('server started');
+})
+
+io.on('connection',socket=>{
+    console.log('socket connected');
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  private socket: Socket;
+
   title = 'sample';
   url = window.location.origin;
+  constructor() {
+    this.socket = io();
+  }
 }
