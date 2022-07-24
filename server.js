187a52fda74e717fcdc357ebb6e9fdfcdf640b8b
Osama Shaikh <shaikhosama504@gmail.com>
2022-07-24 20:07:36 +0530

    log port

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/sample/index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(process.env.PORT);
+});
